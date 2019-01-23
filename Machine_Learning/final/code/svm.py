69d1b2496cb2dd26b901e0952bd3250e673f8136
Philip Lassen <philiplassen@gmail.com>
2019-01-22 18:49:07 -0800

    Possible Paramters for parameter tuning have been added

--- Machine_Learning/final/code/svm.py
+++ Machine_Learning/final/code/svm.py
@@ -30,3 +30,36 @@ log(len(y))
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 
+
+# Need a method for generating G given X and y
+def G(X, y):
+  index0s = [i for i in range(len(y)) if y[i] == 0]
+  index1s = [i for i in range(len(y)) if y[i] == 1]
+  X0 = X[index0s, :]
+  X1 = X[index1s, :]
+  
+
+  G = []
+  for i in range(len(y)):
+    G += [shortestDistance(X[i, :], X1)] if y[i] == 0 else [shortestDistance(X[i, :], X0)]
+  log(G[len(G) - 1])
+  log(G[0])
+  return np.median(G)
+
+
+def shortestDistance(x, X):
+  distances = [np.linalg.norm(x-xi) for xi in X]
+  distances.sort()
+  return distances[0]
+
+theta = G(X, y)
+gamma = 1.0 / (2 * (theta ** 2))
+
+print(theta)
+print(gamma)
+
+b = 2
+
+
+gammas = [gamma * (b ** i) for i in range(-3, 4)]
+Cs = [b ** i for i in range(-1, 4)]
