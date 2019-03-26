b834010f239ca4275fbaa6a27608e02d93628b26
Philip Lassen <philiplassen@gmail.com>
2019-03-26 04:38:04 +0100

    Friend Recommender Componenent has been Added

--- Web_Science/ass1/quizmaster/game3.py
+++ Web_Science/ass1/quizmaster/game3.py
@@ -1,8 +1,15 @@
-#/usr/bin/env python3
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
 
 import sys
-from classifier import cleaned_data as data
+#from classifier import cleaned_data as data
 import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 DEBUG = False
 if '-v' in sys.argv:
   DEBUG = True
@@ -10,5 +17,51 @@ if '-v' in sys.argv:
 def log(message):
   if DEBUG:
     print(message)
+
+
+"""Need to figure out what the best format of the data is"""
+
+df = pd.read_csv("crowd.tsv", encoding = "iso-8859-1", sep = '\t')
+df = df[['id', 'question', 'opinion']]
+df
+
+
+# In[15]:
+
+
+mean = df.groupby(['id'], as_index = False, sort = False).mean().rename(columns = {'opinion' : 'opinion_mean'})[['id', 'opinion_mean']]
+Opinions = pd.merge(df, mean, on = 'id', how = 'left', sort = False)
+Opinions['adjusted'] = Opinions['opinion'] - Opinions['opinion_mean']
+Opinions
+
+
+# In[19]:
+
+
+result = pd.DataFrame({'id':Opinions['id'], "question": Opinions['question'], "opinion" : Opinions['adjusted']})
+result1 = result.pivot_table(index = 'id', columns = 'question', values = 'opinion').fillna(0)
+result1
+
+
+# In[47]:
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+cosine_similarity(result1)
+resulty = np.argsort(cosine_similarity(result1))
+user = 10
+k = 4
+np.flip(resulty[10, -(k+1): 37])
+
+
+# In[26]:
+
+
+
+
+
+# In[ ]:
+
+
 
 
