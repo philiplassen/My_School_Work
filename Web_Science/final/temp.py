8b4a814939f110cd6f4cc690d65038745ba1c6ae
Philip Lassen <philiplassen@gmail.com>
2019-04-02 23:26:38 +0200

    Proccessed Data for Collaborative Filtering

--- Web_Science/final/temp.py
+++ Web_Science/final/temp.py
@@ -11,57 +11,28 @@ def log(message):
   if VERBOSE:
     print(message)
 
-
-"""
-Importing data from different Folders
-"""
 folder = "ml-latest-small/"
+
+
+
 
 linksdf = pd.read_csv(folder + "links.csv")
 moviesdf = pd.read_csv(folder + "movies.csv")
 tagsdf = pd.read_csv(folder + "tags.csv")
 ratingsdf = pd.read_csv(folder + "ratings.csv")
 
-"""
-Logging headers from the different DataFrames
-"""
 log("Links headers are : " + str(list(linksdf)))
 log("Tags headers are : " + str(list(tagsdf)))
 log("Movies headers are : " + str(list(moviesdf)))
 log("Ratings headers are : " + str(list(ratingsdf)))
 
-
-
-"""Merge movies and ratings table"""
 mergedDF = pd.merge(moviesdf, ratingsdf, on = 'movieId', how = 'left', sort = False)
-
-
-"""
-Below are the lines to subtract the mean user rating from movie ratings.
-This is done to get rid of the Rating bias introduced by the diifferent user.
-"""
-mean_user_rating = ratingsdf.groupby(['userId'], as_index = False, sort = False).mean().rename(columns = {'rating' : 'rating_mean'})[['userId', 'rating_mean']]
-RatingsDF  = pd.merge(ratingsdf, mean_user_rating, on = 'userId', how = 'left', sort = False)
-RatingsDF['adjusted'] = RatingsDF['rating'] - RatingsDF['rating_mean']
-CFDF = pd.DataFrame({'userId' : RatingsDF['userId'], 'movieId' : RatingsDF['movieId'], "rating" : RatingsDF["adjusted"]})
-
-"""
-Loggin results of New DataFrame
-"""
-log("New CFDF  headers are : " + str(list(CFDF)))
-log(CFDF)
-
-result = CFDF.pivot_table(index = 'userId', columns = "movieId", values = "rating").fillna(0)
-log(result)
-
-"""
-
-mergedDF = pd.merge(, RatingsDF, on = 'movieId', how = 'left', sort = False)
 result = mergedDF.pivot_table(index = 'userId', columns = 'movieId', values = 'rating')
 result1 = mergedDF.pivot_table(index = 'userId', columns = 'movieId', aggfunc = 'mean')
 log("Merged headers are : " + str(list(mergedDF)))
 log(result)
 log(result1)
+"""
 df = pd.read_csv("crowd.tsv", encoding = "iso-8859-1", sep = '\t')
 df = df[['id', 'question', 'opinion']]
 mean = df.groupby(['id'], as_index = False, sort = False).mean().rename(columns = {'opinion' : 'opinion_mean'})[['id', 'opinion_mean']]
@@ -76,4 +47,5 @@ user = int(input("What is your ID? : "))
 k = int(input("Value for K cut off? : "))
 print(np.flip(resulty[user, -(k+1): 37]))
 """
+
 
